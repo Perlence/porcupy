ff45bf274548187a64438be49318b281156c5c2d
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-01-01 12:02:02 +0300

    Volleybot: Replace some variables with constants

--- volleybot_aliased.py
+++ volleybot_aliased.py
@@ -1,61 +1,62 @@
 from pyegs.runtime import timers, system, yegiks, bots, points
 
+PLAYERS = [yegiks[1], yegiks[2]]
+BALL = yegiks[2]
+
+PLAYER_BOT = bots[2]
+BALL_BOT = bots[3]
+
+NET = points[1]
+WAYPOINT = points[2]
+
+PLAYER_SPAWNS = [1, 4]
+BALL_SPAWNS = [2, 3]
+
 PUNCH_FORCE = -15
 
 if timers[1].value <= 1:
-    players = [yegiks[1], yegiks[2]]  # p1z 1 p2z 2
-    ball = yegiks[2]  # p3z 3
-
-    player_bot = bots[2]  # p4z 2
-    ball_bot = bots[3]  # p5z 3
-
-    net = points[1]  # p6z 1
-    waypoint = points[2]  # p7z 2
-
-    player_spawns = [1, 4]  # p8z 1 p9z 4
-    ball_spawns = [2, 3]  # p10z 2 p11z 3
     ball_speed_x = 0  # p12z 0
-    player_touches = (0, 0)  # p13z 0 p14z 0
+    player_touches = [0, 0]  # p13z 0 p14z 0
 
     system.bots = 2  # yb 2
-    players[0].spawn(player_spawns[0])  # p15z 1+0 p16z 8+0 e^15b p16z
-    players[1].spawn(player_spawns[1])  # p15z 1+0 p16z 8+0 e^15b p16z
-    player_bot.ai = False  # a^5i 0
-    player_bot.goto(waypoint)  # a^5g p7z
+    PLAYERS[0].spawn(PLAYER_SPAWNS[0])  # p15z 1+0 p16z 8+0 e^15b p16z
+    PLAYERS[1].spawn(PLAYER_SPAWNS[1])  # p15z 1+0 p16z 8+0 e^15b p16z
+    PLAYER_BOT.ai = False  # a^5i 0
+    PLAYER_BOT.goto(WAYPOINT)  # a^5g p7z
 
-    ball.spawn(ball_spawns[0])
-    ball_bot.ai = 0
-    ball.speed_y = 0
-    ball.speed_x = 0
+    BALL.spawn(BALL_SPAWNS[0])
+    BALL_BOT.ai = 0
+    BALL.speed_y = 0
+    BALL.speed_x = 0
 
 # Ball movement
-ball.health = 100
-ball.speed_y *= 0.88
+BALL.health = 100
+BALL.speed_y *= 0.88
 
-if ball.speed_x != 0 and ball.speed_y != 0:
-    ball_speed_x = ball.speed_x
+if BALL.speed_x != 0 and BALL.speed_y != 0:
+    ball_speed_x = BALL.speed_x
 
-if ball_speed_x != 0 and ball.speed_x == 0 and ball.speed_y != 0:
+if ball_speed_x != 0 and BALL.speed_x == 0 and BALL.speed_y != 0:
     ball_speed_x *= -0.8
-    ball.speed_x = ball_speed_x
+    BALL.speed_x = ball_speed_x
 
 # Calculate punches
-for player_num, player in enumerate(players):
+for player_num, player in enumerate(PLAYERS):
     if player_touches[player_num] >= 4:
         continue
-    if (player.pos_y - 20 < ball.pos_y < net.pos_y and
-            player.pos_x - 20 < ball.pos_x < player.pos_x + 20):
+    if (player.pos_y - 20 < BALL.pos_y < NET.pos_y and
+            player.pos_x - 20 < BALL.pos_x < player.pos_x + 20):
         player_touches[player_num] += 1
         player_touches[1-player_num] = 0
-        player_ball_distance_x = ball.pos_x - player.pos_x
+        player_ball_distance_x = BALL.pos_x - player.pos_x
 
         if player_ball_distance_x > 0:
-            ball.speed_x = 10
+            BALL.speed_x = 10
 
         if player_ball_distance_x < 0:
-            ball.speed_x = -10
+            BALL.speed_x = -10
 
-        ball.speed_y = PUNCH_FORCE  # e^3v -15
+        BALL.speed_y = PUNCH_FORCE  # e^3v -15
 
 # Check touches
 if player_touches[0] == 3 or player_touches[1] == 3:
@@ -63,19 +64,19 @@ if player_touches[0] == 3 or player_touches[1] == 3:
 
 # Player scores
 player_num_scores = -1
-if ball.pos_y > net.pos_y and ball.pos_x > net.pos_x:
+if BALL.pos_y > NET.pos_y and BALL.pos_x > NET.pos_x:
     player_num_scores = 0
-elif ball.pos_y > net.pos_y and ball.pos_x < net.pos_x:
+elif BALL.pos_y > NET.pos_y and BALL.pos_x < NET.pos_x:
     player_num_scores = 1
 
 if player_num_scores > -1:
     timers[1].value = 5
 
-    players[player_num].frags += 1
+    PLAYERS[player_num].frags += 1
 
-    ball.spawn(ball_spawns[player_num])
-    ball.speed_y = 0
-    ball.speed_x = 0
+    BALL.spawn(BALL_SPAWNS[player_num])
+    BALL.speed_y = 0
+    BALL.speed_x = 0
 
     ball_speed_x = 0
     ball_speed_y = 0
@@ -83,6 +84,6 @@ if player_num_scores > -1:
     player_touches = (0, 0)
 
 # Bot intelligence
-waypoint.pos_y = 425
-waypoint.pos_x = ball.pos_x
-player_bot.goto(2)
+WAYPOINT.pos_y = 425
+WAYPOINT.pos_x = BALL.pos_x
+PLAYER_BOT.goto(2)
