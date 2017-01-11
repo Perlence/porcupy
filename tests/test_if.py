34aea9ab21930407f933ae7827c580bc2a79477a
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-01-11 11:27:10 +0300

    Add a test to check chaining of different bool ops

--- tests/test_if.py
+++ tests/test_if.py
@@ -15,6 +15,8 @@ def test_optimized_if():
     assert compile_('if []: x = 11') == ''
     assert compile_('if [1, 2, 3]: x = 11') == 'p1z 11'
 
+    # assert compile_('x = 11\nif 5 + 7 and True: y = 22') == 'p1z 11 p2z 22'
+
 
 def test_generic_if():
     # Test is Compare
@@ -30,14 +32,18 @@ def test_generic_if():
     # Test is BoolOp
     assert compile_('x = 11\nif x < 12 and x < 13: y = 22') == 'p1z 11 # p1z < 12 & p1z < 13 ( p2z 22 )'
     assert compile_('x = 11\nif x < 12 and x < 13 and x < 14: y = 22') == 'p1z 11 # p1z < 12 & p1z < 13 & p1z < 14 ( p2z 22 )'
-    assert compile_('x = 11\nif x < 12 and x < 13 or x < 14: y = 22') == 'p1z 11 # p1z < 12 & p1z < 13 | p1z < 14 ( p2z 22 )'
+
+    # # Chaining different bool operations is broken in Egiks, resort to
+    # # this workaround instead
+    # assert (compile_('x = 11\nif x < 12 and x < 13 or x < 14: y = 22') ==
+    #         'p1z 11 '
+    #         'p2z 0 # p1z < 12 & p1z < 13 ( p2z 1 ) '
+    #         '# p2z ! 0 | p1z < 13 ( p2z 1 )')
 
     # assert compile_('x = 11\nif x < 5 + 7: y = 22') == 'p1z 11 # p1z < 12 ( p2z 22 )'
     assert compile_('x = 11; y = 22\nif x < y + 7: z = 33') == 'p1z 11 p2z 22 # p1z < p2z+7 ( p3z 33 )'
 
     # Test is BinOp
     assert compile_('x = 1\nif x + 2: y = 22') == 'p1z 1 # p1z+2 ! 0 ( p2z 22 )'
-    # # Test is UnaryOp
-    # assert compile_('x = -1\nif -x: y = 22') == 'p1z -1 # p1z*-1 ! 0 ( p2z 22 )'
-
-    # assert compile_('x = 11\nif 5 + 7 and True: y = 22') == 'p1z 11 # 12 ! 0 & 1 ! 0 ( p2z 22 )'
+    # Test is UnaryOp
+    assert compile_('x = -1\nif -x: y = 22') == 'p1z -1 # p1z*-1 ! 0 ( p2z 22 )'
