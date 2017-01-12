41c3adfd3c151cfea300d50699b9028302a3a061
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-01-12 15:45:23 +0300

    Add tests for 'break' and 'continue' keywords

--- tests/test_for.py
+++ tests/test_for.py
@@ -5,11 +5,34 @@ from pyegs.compiler import compile as compile_
 
 @pytest.mark.skip('Not implemented yet')
 def test_for():
-    assert (compile_('items = [11, 22, 33]\nfor item in items: x = item') ==
+    assert (compile_('items = [11, 22, 33]\n'
+                     'for item in items:\n'
+                     '    x = item') ==
             'p1z 11 p2z 22 p3z 33 p4z 1 '
-            'p5z p4z+0 p5z p^5z p6z p5z '
-            'p5z p4z+1 p5z p^5z p6z p5z '
-            'p5z p4z+2 p5z p^5z p6z p5z')
+            'p7z p4z+0 p5z p^7z p6z p5z '
+            'p7z p4z+1 p5z p^7z p6z p5z '
+            'p7z p4z+2 p5z p^7z p6z p5z')
+
+    assert (compile_('items = [11, 22, 33]\n'
+                     'for item in items:\n'
+                     '    if item > 20:\n'
+                     '        break') ==
+            'p1z 11 p2z 22 p3z 33 p4z 1 '
+            'p6z p4z+0 p6z p^6z p6z 0 # p6z > 20 ( p6z 1 ) # p6z ! 0 ( g2z ) '
+            'p6z p4z+1 p6z p^6z p6z 0 # p6z > 20 ( p6z 1 ) # p6z ! 0 ( g2z ) '
+            'p6z p4z+2 p6z p^6z p6z 0 # p6z > 20 ( p6z 1 ) # p6z ! 0 ( g2z ) '
+            ':2')
+
+    assert (compile_('items = [11, 22, 33]\n'
+                     'for item in items:\n'
+                     '    if item < 20:\n'
+                     '        continue\n'
+                     '    y = 22') ==
+            'p1z 11 p2z 22 p3z 33 p4z 1 '
+            'p6z p4z+0 p6z p^6z p7z 1 p8z 0 # p6z < 20 ( p8z 1 ) # p8z ! 0 ( p7z 0 ) # p7z ! 0 ( p5z 22 ) '
+            'p6z p4z+1 p6z p^6z p7z 1 p8z 0 # p6z < 20 ( p8z 1 ) # p8z ! 0 ( p7z 0 ) # p7z ! 0 ( p5z 22 ) '
+            'p6z p4z+1 p6z p^6z p7z 1 p8z 0 # p6z < 20 ( p8z 1 ) # p8z ! 0 ( p7z 0 ) # p7z ! 0 ( p5z 22 ) '
+            ':2')
 
     assert (compile_('for x in range(5): y = x') ==
             'p1z 0 p2z p1z '
