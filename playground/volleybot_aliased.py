10c0fbfb34f410d8b31a130d0ac0dc5a13854666
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-01-17 21:06:28 +0300

    Volleybot: Make use of 'continue'

--- playground/volleybot_aliased.py
+++ playground/volleybot_aliased.py
@@ -10,7 +10,8 @@ WAYPOINT = points[2]
 PLAYER_SPAWNS = [1, 4]
 BALL_SPAWNS = [2, 3]
 
-PUNCH_FORCE = -15
+PUNCH_VELOCITY_X = 10
+PUNCH_VELOCITY_Y = -15
 
 if timers[1].value == 1:
     player_touches = [0, 0]
@@ -40,27 +41,26 @@ if ball_speed_x != 0 and BALL.speed_x == 0 and BALL.speed_y != 0:
 # for player_num, player in enumerate(PLAYERS):
 player_num = 0
 for player in PLAYERS:
-    # if player_touches[player_num] >= 4:
-    #     continue
-    if player_touches[player_num] < 4:
-        if (player.pos_y - 20 < BALL.pos_y < NET.pos_y and
-                player.pos_x - 20 < BALL.pos_x < player.pos_x + 20):
-            player_touches[player_num] += 1
-            player_touches[1-player_num] = 0
-            player_ball_distance_x = BALL.pos_x - player.pos_x
+    if player_touches[player_num] >= 3:
+        continue
+    if (player.pos_y - 20 < BALL.pos_y < NET.pos_y and
+            player.pos_x - 20 < BALL.pos_x < player.pos_x + 20):
+        player_touches[player_num] += 1
+        player_touches[1-player_num] = 0
+        player_ball_distance_x = BALL.pos_x - player.pos_x
 
-            if player_ball_distance_x > 0:
-                BALL.speed_x = 10
+        BALL.speed_x = PUNCH_VELOCITY_X
+        if player_ball_distance_x < 0:
+            BALL.speed_x *= -1
 
-            if player_ball_distance_x < 0:
-                BALL.speed_x = -10
-
-            BALL.speed_y = PUNCH_FORCE
-        player_num += 1
+        BALL.speed_y = PUNCH_VELOCITY_Y
+    player_num += 1
 
 # Check touches
 if player_touches[0] == 3 or player_touches[1] == 3:
-    system.message_at(282, 100, 50, '3 касания!')
+    system.color = 188
+    system.message_at(282, 100, 50, '3 kasaniya!')
+    system.color = 200*256
 
 # Player scores
 player_num_scores = -1
@@ -70,11 +70,9 @@ elif BALL.pos_y > NET.pos_y and BALL.pos_x < NET.pos_x:
     player_num_scores = 1
 
 if player_num_scores > -1:
-    timers[1].value = 5
+    PLAYERS[player_num_scores].frags += 1
 
-    PLAYERS[player_num].frags += 1
-
-    BALL.spawn(BALL_SPAWNS[player_num])
+    BALL.spawn(BALL_SPAWNS[player_num_scores])
     ball_speed_x = BALL.speed_x = BALL.speed_y = 0
 
     # player_touches = [0, 0]
